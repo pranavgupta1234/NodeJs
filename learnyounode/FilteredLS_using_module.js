831cf8b6c31b7cd605ea23aa7f3a959965acb900
pranavgupta1234 <pranavgupta4321@gmail.com>
2017-01-24 21:05:09 +0530

    my first module for directory search using modular programming + documentation added

--- learnyounode/FilteredLS_using_module.js
+++ learnyounode/FilteredLS_using_module.js
@@ -1,6 +1,12 @@
-var filteredList =  require('./module_for_dirSearch');
+var dirSearch =  require('./module_for_dirSearch');
 
-console.log(filteredList(process.argv[2],process.argv[3],function callback(err,data){
+/* Here we are just calling the function from module which we just required
+Note that the callback function defined here by us is passed to the function in the module and it will be called with
+the arguments as defined in the api provided by whose module you are using(you need to give it that way)
+He can simply call this callback function as callback(null,DataHeComputed);
+so the data computed by hin is implemented by you as below
+*/
+dirSearch(process.argv[2],process.argv[3],function callback(err,data){
   if(err){
     console.log("Error!!!");
   }
@@ -9,4 +15,4 @@ console.log(filteredList(process.argv[2],process.argv[3],function callback(err,d
       console.log(data[i]);
     }
   }
-}));
+});
--- learnyounode/module_for_dirSearch.js
+++ learnyounode/module_for_dirSearch.js
@@ -1,14 +1,21 @@
 var fs = require('fs');
 var path = require('path');
-
-var AllFiles = [];
+var AllFiles= [];
 //var folderPath = process.argv[2];
 //var extension = "."+ process.argv[3];
+/* Firstly we just made a functional expression accepting three arguments (third one is the callback func implemeted by user and the
+maker of module should call it as he has said in the api)
+Now we have to internally use fs module and then use our callBack function get required data(end user doesn't mind this)
+(it has to be implemented according to api of fs)
+Now after formulating the data as we wanted we will invoke callBack function with proper parameters and result will be printed
+and if any error occures during fs module we can simply invoke error to the user
+Note that by convention err is always first argument to callbacks and calling the callback function with just err is no harm
+*/
 var dirSearch = function(dirName,extension,callback){
-  fs.readdir(dirName,function callback(err,data){
+  fs.readdir(dirName,function callBack(err,data){
     if(err){
       //console.log("Sorry ! Error Occured");
-      return console.error(err);
+      return callback(err);
     }
     else{
       var i=0;
@@ -18,28 +25,9 @@ var dirSearch = function(dirName,extension,callback){
           i++;
           //console.log(file);
         }
-      })
-      return AllFiles;
+      });
+      callback(null,AllFiles);
     }
   });
 };
-// var dirSearch = function (dirName,extension,function callback(err,data){
-//     fs.readdir(folder,function(err,data){
-//       if(err){
-//         //console.log("Sorry ! Error Occured");
-//         return console.error(err)
-//       }
-//       else{
-//         var i=0;
-//         data.forEach(function(file){
-//           if(path.extname(file)==extension){
-//             AllFiles[i]=file;
-//             i++;
-//             //console.log(file);
-//           }
-//         })
-//         return AllFiles;
-//       }
-//     });
-// })
 module.exports = dirSearch;
